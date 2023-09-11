67aab09240aaef0505a391a221c236666dc759b6
MaherWasel <132270545+MaherWasel@users.noreply.github.com>
2023-09-12 02:12:25 +0300

    fixing loading exceptions

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -13,13 +13,12 @@ class HomeScreen extends StatefulWidget{
 }
 class _HomeScreenState extends State<HomeScreen>{
   bool isloading =true;
-  
-  var _intialV1="en";
-  var _intialV2="ar";
+  final Map<String,String> inOutSentences={};
+
   final textSentenceController=TextEditingController();
   final textKeyController=TextEditingController();
    String translated="";
-  bool connectedSuccess=false;
+  bool loading=false;
    final Map<dynamic,dynamic> listOfLang={};
    final listed=["am","da"];
   bool intilized=false;
@@ -30,18 +29,33 @@ class _HomeScreenState extends State<HomeScreen>{
   
   }
   void submited()async {
+    final sentenceText=textSentenceController.text;
+    final key=textKeyController.text;
     if (listOfLang.isEmpty){
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Check your Internet Connection"),duration: Duration(seconds: 2),));
+        setState(() {
+          loading=false;
+
+  });
       return;
     }
-    else if (textSentenceController.text.isEmpty || textKeyController.text.isEmpty){
-        const SnackBar(content: Text("Empty Input"),duration: Duration(seconds: 2),);
+    else if (sentenceText=="" || key==""){
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("Empty input"),duration: Duration(seconds: 2),))   ;
+             setState(() {
+          loading=false;
+
+  });
 
 
     }
     else if (!listOfLang.keys.contains(textKeyController.text)){
-        const SnackBar(content: Text("Invalid Key"),duration: Duration(seconds: 2),);
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text("invalid key"),duration: Duration(seconds: 2),));  setState(() {
+          loading=false;
+
+  });
 
     }
     else {
@@ -61,20 +75,34 @@ class _HomeScreenState extends State<HomeScreen>{
 
     setState(() {
          translated=data["data"]["translatedText"];
+         inOutSentences[textSentenceController.text]=translated;
+               loading=false;
+
 
     });
 
   }
+  setState(() {
+          loading=false;
+
+  });
  
     }
     
   }
   void collectingData() async{
+    setState(() {
+      loading=true;
+    });
     final url =Uri.tryParse("https://text-translator2.p.rapidapi.com/getLanguages");
     final response = await http.get(url!,headers: {"X-RapidAPI-Key": "b208eec911mshcf2bbc911339699p19588cjsn4fa2db4a3d06",
 	"X-RapidAPI-Host": "text-translator2.p.rapidapi.com"
     });
     if (response.statusCode!=200){
+      setState(() {
+        loading=false;
+      });
+
       return;
     }
     final data=jsonDecode(response.body);
@@ -83,6 +111,7 @@ class _HomeScreenState extends State<HomeScreen>{
        for (int i =0;i<list.length;i++){
         listOfLang[data["data"]["languages"][i]["code"].toString()]=data["data"]["languages"][i]["name"].toString();
     }
+    loading=false;
     });
    
   
@@ -127,8 +156,22 @@ class _HomeScreenState extends State<HomeScreen>{
         ),
         child: Column(
             children: [
+              loading?const SizedBox(
+                height: 200,
+                width: 200,
+                child: CircularProgressIndicator(
+                  
+                ),
+              ):
               InkWell(
-                onTap: submited,
+                onTap: (){
+                  setState(() {
+                    FocusManager.instance.primaryFocus?.unfocus();
+                    loading=true;
+                    submited();
+                    
+                  });
+                },
                 child: Image.asset("assets/images/one.webp",
                 scale: 1.6,
                 )),
@@ -144,10 +187,12 @@ class _HomeScreenState extends State<HomeScreen>{
                   children: [
                     Expanded(
                       child: TextField(
-                        maxLength: 50,
+                        minLines: 3,
+                        maxLines: 3,
+                        maxLength: 100,
                         controller: textSentenceController,
                         decoration: InputDecoration(
-                          label: Text("Enter Your Sentence",
+                          label: Text("Enter Your Sentence \n",
                           style: GoogleFonts.lato(
                             fontSize: 18
                           ),)
@@ -158,10 +203,13 @@ class _HomeScreenState extends State<HomeScreen>{
                       width: 140,
                       child: TextField(
                         controller: textKeyController,
+                        minLines: 3,
+                        maxLines: 3,
                         maxLength: 3,
                         decoration: InputDecoration(
                           
                           label: Text("Enter Language key",
+                          
                           style: GoogleFonts.lato(
                             fontSize: 12,
                           ),)
@@ -182,7 +230,8 @@ class _HomeScreenState extends State<HomeScreen>{
                   color: const Color.fromARGB(255, 211, 208, 208),
                   borderRadius: BorderRadius.circular(15)
                 ),
-                child: Center(child:  Text(translated.isEmpty?"empty":translated)),
+                child: Center(child:  Text(translated.isEmpty?"empty":translated,
+                style: const TextStyle(fontSize: 22),)),
               )
             ],
       
